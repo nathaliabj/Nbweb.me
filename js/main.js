7f47906eb3dcd7ec255c8276b485c905491e3f59
nathaliabj <nathalia.buitragoj@gmail.com>
2018-08-18 18:45:06 +0100

    Profile

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
-TweenMax.staggerTo(".bg-line", .8, {height:'100%'}, 0.3);
-
+// animates the color lines on the skills
 var tl = new TimelineLite();
-tl.to("header", .5, {opacity:1, top:0}, "+=1")
-  .to("#content h1", .5, {opacity:1, top:0}, "-=.3")
-  .to("#content p", .5, {opacity:1, top:0}, "-=.3")
-  .to("#content h2", .5, {opacity:1, top:0}, "-=.3")
-  .to(".secondary", .5, {opacity:1, top:0}, "-=.3")
-  .to(".bar-color", .5, {width: '100%', background:'#ffafb4', left:0}, "-=.3")
-  .to("#shapes", .5, {opacity:1, top:0}, "-=.3")
-  .to("#footer", .5, {opacity:1, top:0}, "-=.3");
+tl.to(".bar-color", .5, {width: '100%', background:'#ffafb4', left:0}, "2.2")
 
-  if(navigator.userAgent.indexOf('Safari') !=-1 && navigator.userAgent.indexOf('Chrome') == -1)
-  {
-    var element = document.getElementById("body");
-  element.classList.add("safari");
-  }
+// loop to animate the background lines
+$(".bg-line").each(function(index){
+  $(this).delay(300 * index).animate({height:"100%"}, 800);
+});
+
+// animates header
+$("header").delay(1100).animate({opacity:1, top:0}, 500);
+
+// animates the h1
+$("#content h1").delay(1300).animate({opacity:1, top:0}, 500);
+
+// animates the main paragraph
+$("#content p").delay(1500).animate({opacity:1, top:0}, 500);
+
+// animates the h2
+$("#content h2").delay(1700).animate({opacity:1, top:0}, 500);
+
+// animates the secondary content
+$(".secondary").delay(1900).animate({opacity:1, top:0}, 500);
+
+// animates the shape
+$("#shapes").delay(2300).animate({opacity:1, top:0}, 500);
+
+// animates the footer
+$("#footer").delay(2500).animate({opacity:1, top:0}, 500);
+
+// loop to fade the gallery when the page loads
+$( ".portfolio li" ).each(function( index ) {
+  $(this).fadeTo(0, 0).delay(150 * index).fadeTo(400, 1, 'swing');
+});
